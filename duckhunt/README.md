64b719026f9c4453f7700285f0d75b60f6700ec2
TamLuong <luongantam@gmail.com>
2014-02-23 13:57:07 -0500

    add readme file

---
+++ duckhunt/README.md
@@ -0,0 +1,11 @@
+Assignment 2 Duckhunt
+
+1. I think I have drawn all the required images for this assignment.
+
+2. Person(s) with whom I have collaborated or discussed the assignment with: none.
+
+3. Hours spent on this assignment: 3
+
+4. Additional comments: this is a very fun assignment, I am looking forward to doing the animation for this. 
+Also, I wonder how when drawImage() is rendered in the canvas, it can trim the extra gray spaces 
+around the small images from the png file.
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,47 @@
 // Your work goes here...
+
+function draw() {
+
+	var canvas = document.getElementById('game');
+
+	if (canvas.getContext){
+  		var ctx = canvas.getContext('2d');
+  			
+  			// drawing dirt road
+  			//ctx.fillStyle = "#C96A1B";
+    		//ctx.fillRect (0, 450, 800, 150);
+    		
+    		var img = new Image();
+    		img.onload = function() {
+    		
+    			//draw tree
+    			ctx.drawImage(img,0,270,80,130,0,100,250,400);
+    			
+    			//draw bushes and dirt road
+    			ctx.drawImage(img,0,710,900,180,0,350,800,250);
+    			
+    			//draw dog sniffing
+    			ctx.drawImage(img,0,0,60,45,50,500,120,90);
+    			
+    			//draw birds
+    			ctx.drawImage(img,0,118,35,35,300,100,70,70);
+    			ctx.drawImage(img,130,118,35,35,400,100,70,70);
+    			ctx.drawImage(img,260,118,35,35,500,100,70,70);
+    			ctx.drawImage(img,40,156,35,35,300,200,70,70);
+    			ctx.drawImage(img,300,194,35,35,400,200,70,70);
+    			
+    			
+    			
+    		};
+    		img.src = "assets/duckhunt.png";
+
+  
+  
+  
+  
+  
+	} else {
+  		// canvas-unsupported code here
+	}
+
+}
