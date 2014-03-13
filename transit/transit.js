ee9a834233ca6cb3268ce39d1252b2aee15b1dbe
TamLuong <luongantam@gmail.com>
2014-03-13 00:23:50 -0400

    adding revised file 5

--- transit/transit.js
+++ transit/transit.js
@@ -2,7 +2,7 @@
       function initialize() {
         var mapOptions = {
           center: new google.maps.LatLng(42.418333, -71.106667),
-          zoom: 8
+          zoom: 12
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
